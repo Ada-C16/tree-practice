b2cfa2ff17bed91e4a91f45325f73eab539462ff
Areeba Rashid <77311713+AreebaQ@users.noreply.github.com>
2022-04-10 19:09:08 -0700

    add method works others have failing tests

--- binary_search_tree/tree.py
+++ binary_search_tree/tree.py
@@ -14,20 +14,55 @@ class Tree:
     def __init__(self):
         self.root = None
 
-    # Time Complexity: 
-    # Space Complexity: 
-    def add(self, key, value = None):
-        pass
+    def addHelper(self, current_root, new_node):
+        if current_root is None:
+            return new_node
+        
+        if new_node.key < current_root.key:
+            current_root.left = self.addHelper(current_root.left, new_node)
+        else:
+            current_root.right = self.addHelper(current_root.right, new_node)
 
     # Time Complexity: 
     # Space Complexity: 
+    def add(self, key, value = None):
+        new_node = TreeNode(key, value)
+
+        if self.root is None:
+            self.root = new_node
+            return 
+        self.addHelper(self.root, new_node)
+
+    
+    def findHelper(self, current_node, key):
+        if not current_node:
+            return None
+        if current_node.key == key:
+            return current_node.value
+        if current_node.key > key:
+            return self.findHelper(current_node.left, key)
+        if current_node.key < key:
+            return self.findHelper(current_node.right, key)    
+
+    # Time Complexity: 
+    # Space Complexity:
     def find(self, key):
-        pass
+        #if it's empty return None
+        return self.findHelper(self.root, key)
 
     # Time Complexity: 
     # Space Complexity: 
     def inorder(self):
-        pass
+        result = []
+        if self.root.left:
+            result += self.root.left.inorder()
+
+        result.append(self.root.value)
+
+        if self.root.right:
+            result += self.root.right.inorder()
+
+        return result
 
     # Time Complexity: 
     # Space Complexity:     
