c7dbb64983a8573d519b4887c0ce32af93498f64
Jessica <jessica.w.roberts@gmail.com>
2022-04-12 11:44:21 -0700

    Update to pass tests

--- binary_search_tree/tree.py
+++ binary_search_tree/tree.py
@@ -14,42 +14,129 @@ class Tree:
     def __init__(self):
         self.root = None
 
-    # Time Complexity: 
-    # Space Complexity: 
+    def add_helper(self, current_root, new_node):
+        if current_root == None:
+            return new_node
+        if new_node.key < current_root.key:
+            current_root.left = self.add_helper(current_root.left, new_node)
+        else:
+            current_root.right = self.add_helper(current_root.right, new_node)
+        return current_root
+
+    # Time Complexity:  O(log n)
+    # Space Complexity: O(log n)
     def add(self, key, value = None):
-        pass
+        new_node = TreeNode(key, value)
 
-    # Time Complexity: 
-    # Space Complexity: 
+        if self.root is None:
+            self.root = new_node
+            return
+        self.add_helper(self.root, new_node)
+        
+
+    def find_helper(self, current, key):
+        if current == None:
+            return None
+        elif current.key == key:
+            return current.value
+        elif current.key > key:
+            return self.find_helper(current.left, key)
+        elif current.key < key:
+            return self.find_helper(current.right, key)
+
+    # Time Complexity: O(log n) -- Assuming it's balanced
+    # Space Complexity: O(log n)
     def find(self, key):
-        pass
+        if  self.root == None:
+            return None
+        return self.find_helper(self.root, key)
+        
 
-    # Time Complexity: 
-    # Space Complexity: 
+    def inorder_helper(self, current, items):
+        if current != None:
+            self.inorder_helper(current.left, items)
+            items.append({"key": current.key, "value": current.value})
+            self.inorder_helper(current.right, items)
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
     def inorder(self):
-        pass
+        items = []
+        self.inorder_helper(self.root, items)
+        return items
 
-    # Time Complexity: 
-    # Space Complexity:     
+    def preorder_helper(self, current, items):
+        if current != None:
+            items.append({"key": current.key, "value": current.value})
+            self.preorder_helper(current.left, items)
+            self.preorder_helper(current.right, items)
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)  
     def preorder(self):
-        pass
+        items = []
+        self.preorder_helper(self.root, items)
+        return items
 
-    # Time Complexity: 
-    # Space Complexity:     
+    def postorder_helper(self, current, items):
+        if current != None:
+            self.postorder_helper(current.left, items)
+            self.postorder_helper(current.right, items)
+            items.append({"key": current.key, "value": current.value})
+
+    # Time Complexity: O(n) 
+    # Space Complexity: O(n)  
     def postorder(self):
-        pass
+        items = []
+        self.postorder_helper(self.root, items)
+        return items
 
-    # Time Complexity: 
-    # Space Complexity:     
+    def height_helper(self, current):
+        if current != None:
+            height = max(self.height_helper(current.left), self.height_helper(current.right))
+            return height + 1
+        else: 
+            return 0
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)  
     def height(self):
-        pass
-
-
+        if self.root is None:
+            return 0
+        return self.height_helper(self.root)
+        
+        
+    def bfs_helper(self, current, to_visit):
+        if current.left != None:
+            to_visit.append(current.left)
+        if current.right != None:
+            to_visit.append(current.right)
+    
 #   # Optional Method
-#   # Time Complexity: 
-#   # Space Complexity: 
+#   # Time Complexity: O(n)
+#   # Space Complexity: O(n)
     def bfs(self):
-        pass
+        items = []
+        to_visit = []
+
+        if self.root == None:
+            return items
+
+        items.append({"key": self.root.key, "value": self.root.value})
+        self.bfs_helper(self.root, to_visit)
+
+        while to_visit: 
+            current = to_visit.pop(0)
+            items.append({"key": current.key, "value": current.value})
+            self.bfs_helper(current, to_visit)
+        return items
+
+
+
+
+
+
+
 
         
 
