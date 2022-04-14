d6d70f0a64ab93e815bf79c6cf3e0a0e8be600e1
Symone Blades <symdblad@amazon.com>
2022-04-13 21:16:34 -0700

    4/18 tests passing

--- binary_search_tree/tree.py
+++ binary_search_tree/tree.py
@@ -1,3 +1,6 @@
+from xml.etree.ElementPath import find
+
+
 class TreeNode:
     def __init__(self, key, val = None):
         if val == None:
@@ -16,18 +19,54 @@ class Tree:
 
     # Time Complexity: 
     # Space Complexity: 
-    def add(self, key, value = None):
-        pass
+    def add(self, key, value = None):        
+        if not self.root:
+            self.root = TreeNode(key, value)
+        else:
+            new_node = TreeNode(key, value)
+            current = self.root
+            if new_node.key < current.key:
+                if not current.left:
+                    current.left = new_node 
+                    return
+                elif not current.right:
+                    current.right = new_node 
+                    return            
+
 
     # Time Complexity: 
     # Space Complexity: 
     def find(self, key):
-        pass
+        current = self.root
+
+        while current:
+            if current.key == key:
+                return current.value
+            elif key < current.key:
+                current = current.left
+            else:
+                current = current.right
+        return None                 
+
+    def inorder_helper(self, current, values):
+        if not current:
+            return values
+
+        self.inorder_helper(current.left, values)
+        values.append({
+            "key": current.key,
+            "value": current.value
+        })
+
+        self.inorder_helper(current.right, values)
+        return values
 
     # Time Complexity: 
     # Space Complexity: 
     def inorder(self):
-        pass
+        values = [] 
+        return self.inorder_helper(self.root, values)
+
 
     # Time Complexity: 
     # Space Complexity:     
