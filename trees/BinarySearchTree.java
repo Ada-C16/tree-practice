6a047af46e7ce5ed8d3e48d4dc8dac3fb2c59dfc
Sophia Le <sophia.hue.le@gmail.com>
2022-04-14 15:28:41 -0700

    Adds trees in java

---
+++ trees/BinarySearchTree.java
@@ -0,0 +1,246 @@
+package net.sophiale.trees;
+
+import com.sun.source.tree.Tree;
+
+import java.sql.Array;
+import java.util.*;
+
+public class BinarySearchTree<T extends Comparable> implements GeneralTree<T> {
+    TreeNode<T> mRoot;
+
+    @Override
+    public void addChildNode(T value) {
+        if (mRoot == null) {
+            mRoot = new TreeNode<>(value);
+            return;
+        }
+
+        addRecursive(mRoot, value);
+    }
+
+    private void addRecursive(TreeNode<T> cur, T value) {
+        if (value.compareTo(cur.val) == -1) {
+            if (cur.children.get(0) == null) {
+                TreeNode<T> node = new TreeNode<>(value);
+                cur.children.add(0, node);
+                return;
+            }
+            addRecursive(cur.children.get(0), value);
+        } else {
+            if (cur.children.get(1) == null) {
+                TreeNode<T> node = new TreeNode<>(value);
+                cur.children.add(1, node);
+                return;
+            }
+            addRecursive(cur.children.get(1), value);
+        }
+    }
+
+    @Override
+    public void removeChildNode(T value) {
+
+    }
+
+    @Override
+    public boolean findNode(T value) {
+        TreeNode<T> root = mRoot;
+        return findRecursive(root, value);
+    }
+
+    private boolean findRecursive(TreeNode<T> cur, T value) {
+        while (cur != null) {
+            if (value.compareTo(cur.val) == 0) {
+                return true;
+            }
+            if (value.compareTo(cur.val) == -1) {
+                return findRecursive(cur.children.get(0), value);
+            }
+            return findRecursive(cur.children.get(1), value);
+        }
+        return false;
+    }
+
+    @Override
+    public int getHeight() {
+        if (mRoot != null) {
+            return getHeightBFS(mRoot);
+        }
+        return 0;
+    }
+
+    private int getHeightBFS(TreeNode<T> root) {
+        Queue<TreeNode<T>> queue = new LinkedList<TreeNode<T>>();
+        queue.offer(root);
+        int height = 0;
+        queue.offer(null);
+        while (!queue.isEmpty()) {
+            TreeNode<T> node = queue.poll();
+            if (node == null) {
+                if (!queue.isEmpty()) {
+                    queue.offer(null);
+                }
+                height++;
+            } else {
+                if (node.children.get(0) != null) {
+                    queue.offer(node.children.get(0));
+                }
+                if (node.children.get(1) != null) {
+                    queue.offer(node.children.get(1));
+                }
+            }
+        }
+        return height;
+    }
+
+    private int getHeightDFSRecursive(TreeNode<T> node) {
+        if (node == null) {
+            return 0;
+        }
+        return 1 + Math.max(getHeightDFS(node.children.get(0)), getHeightDFS(node.children.get(1)));
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public List<T> preorder() {
+        return preorderRecursive(mRoot, new ArrayList<T>());
+    }
+
+    private List<T> preorderRecursive(TreeNode<T> node, List<T> values) {
+        if (node == null) {
+            return values;
+        }
+        values.add(node.val);
+        preorderRecursive(node.children.get(0), values);
+        preorderRecursive(node.children.get(1), values);
+
+        return values;
+    }
+
+    private List<T> preorderIterative(TreeNode<T> root) {
+        List<T> values = new ArrayList<T>();
+        if (root == null) {
+            return values;
+        }
+        Stack<TreeNode<T>> stack = new Stack<TreeNode<T>>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode<T> node = stack.pop();
+            values.add(node.val);
+
+            if (node.children.get(0) != null) {
+                stack.push(node.children.get(0));
+            }
+
+            if (node.children.get(1) != null) {
+                stack.push(node.children.get(1));
+            }
+        }
+        return values;
+    }
+
+    @Override
+    public List<T> inorder() {
+        return inorderRecursive(mRoot, new ArrayList<T>());
+    }
+
+    private  List<T> inorderRecursive(TreeNode<T> node, List<T> values) {
+        if (node == null) {
+            return values;
+        }
+        inorderRecursive(node.children.get(0), values);
+        values.add(node.val);
+        inorderRecursive(node.children.get(1), values);
+
+        return values;
+    }
+
+    private List<T> inorderIterative(TreeNode<T> root) {
+        List<T> values = new ArrayList<T>();
+        if (root == null) {
+            return values;
+        }
+        Stack<TreeNode<T>> stack = new Stack<TreeNode<T>>();
+        TreeNode<T> node = root;
+        while (!stack.isEmpty() || node != null) {
+            if (node != null) {
+                stack.push(node);
+                node = node.children.get(0);
+            } else {
+                node = stack.pop();
+                values.add(node.val);
+                node = node.children.get(1);
+            }
+        }
+        return values;
+    }
+
+    @Override
+    public List<T> postorder() {
+        return postorderRecursive(mRoot, new ArrayList<T>());
+    }
+
+    private List<T> postorderRecursive(TreeNode<T> node, List<T> values) {
+        if (node == null) {
+            return values;
+        }
+        postorderRecursive(node.children.get(0), values);
+        postorderRecursive(node.children.get(1), values);
+        values.add(node.val);
+
+        return values;
+    }
+
+    private List<T> postorderIterative(TreeNode<T> root) {
+        List<T> values = new ArrayList<T>();
+        if (root == null) {
+            return values;
+        }
+        Stack<TreeNode<T>> stack = new Stack<TreeNode<T>>();
+        Stack<TreeNode<T>> rightStack = new Stack<TreeNode<T>>();
+        TreeNode<T> node = root;
+        while (!stack.isEmpty() || node != null) {
+            if (node != null) {
+                if (node.children.get(0) != null) {
+                    rightStack.push(node.children.get(0));
+                }
+                stack.push(node);
+                node = node.children.get(1);
+            } else {
+                node = stack.peek();
+                if (!rightStack.isEmpty() && node.children.get(0) == rightStack.peek()) {
+                    node = rightStack.pop();
+                } else {
+                    values.add(node.val);
+                    stack.pop();
+                    node = null;
+                }
+            }
+        }
+        return values;
+    }
+
+    @Override
+    public List<T> bfs() {
+        List<T> values = new ArrayList<T>();
+        if (mRoot != null) {
+            Queue<TreeNode<T>> queue = new LinkedList<TreeNode<T>>();
+            queue.offer(mRoot);
+            while (!queue.isEmpty()) {
+                TreeNode<T> node = queue.poll();
+                values.add(node.val);
+                if (node.children.get(0) != null) {
+                    queue.offer(node.children.get(0));
+                }
+                if (node.children.get(1) != null) {
+                    queue.offer(node.children.get(1));
+                }
+            }
+        }
+        return values;
+    }
+
+}
---
+++ trees/notes.md
@@ -0,0 +1,12 @@
+In this exercise you will implement, in Ruby, several Tree methods.
+Notes: the real comparator in this exercise will be key, not value. 
+So in this implementation, we'll consider key the comparator of value
+add(value) - This method adds a key-value pair to the Binary Search Tree
+find(value) - This method returns the matching value for the given key if it is in the tree and None if the key is not found in the tree.
+Traversals: These methods return an array of dictionaries consisting of all the key-value pairs in the tree in a specific order.
+inorder - This method returns an array of all the elements in the tree, in order.
+postorder - This method returns an array of all the elements in a postorder fashion (left, right , root).
+preorder - This method returns an array of all the elements in a preorder fashion (root, left, right).
+height - This method returns the height of the binary search tree.
+Optionals
+bfs - This method returns an array with the tree elements in a level-by-level order (see Breadth-First traversal)
