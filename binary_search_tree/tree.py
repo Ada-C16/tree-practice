9faa175adaeb07f580c128151375c90b722497a0
Symone Blades <symdblad@amazon.com>
2022-04-14 17:18:55 -0700

    All required tests pass

--- binary_search_tree/tree.py
+++ binary_search_tree/tree.py
@@ -10,35 +10,39 @@ class TreeNode:
         self.value = val
         self.left = None
         self.right = None
-        
 
 
 class Tree:
     def __init__(self):
         self.root = None
 
-    # Time Complexity: 
-    # Space Complexity: 
+    # Time Complexity: O(log n)
+    # Space Complexity: O(n)
     def add(self, key, value = None):        
         if not self.root:
             self.root = TreeNode(key, value)
-        else:
-            new_node = TreeNode(key, value)
-            current = self.root
+            return
+        new_node = TreeNode(key, value)
+        current = self.root
+        while current: 
             if new_node.key < current.key:
-                if not current.left:
-                    current.left = new_node 
+                if current.left == None:
+                    current.left = new_node
                     return
-                elif not current.right:
-                    current.right = new_node 
-                    return            
+                else:
+                    current = current.left 
+            else: 
+                if current.right == None:
+                    current.right = new_node
+                    return
+                else:
+                    current = current.right
 
 
-    # Time Complexity: 
-    # Space Complexity: 
+    # Time Complexity: O(log n)
+    # Space Complexity: O(1)
     def find(self, key):
         current = self.root
-
         while current:
             if current.key == key:
                 return current.value
@@ -46,53 +50,89 @@ class Tree:
                 current = current.left
             else:
                 current = current.right
-        return None                 
+        return None     
 
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
+    def inorder(self):
+        values = []
+        self.inorder_helper(self.root, values)
+        return values
+
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)   
+    def preorder(self):
+        values = []
+        self.preorder_helper(self.root, values)
+        return values
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(n) 
+    def postorder(self):
+        values = []
+        self.postorder_helper(self.root, values)
+        return values
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
+    def height(self):
+        return self.height_helper(self.root)
+
+
+#   # Optional Method
+    def bfs(self):
+        pass
+
+#   # Useful for printing
+    def to_s(self):
+        return f"{self.inorder()}"
+
+#  # Helper Functions
     def inorder_helper(self, current, values):
-        if not current:
+        if current is None:
             return values
-
         self.inorder_helper(current.left, values)
         values.append({
             "key": current.key,
             "value": current.value
         })
-
         self.inorder_helper(current.right, values)
         return values
 
-    # Time Complexity: 
-    # Space Complexity: 
-    def inorder(self):
-        values = [] 
-        return self.inorder_helper(self.root, values)
 
-
-    # Time Complexity: 
-    # Space Complexity:     
-    def preorder(self):
-        pass
-
-    # Time Complexity: 
-    # Space Complexity:     
-    def postorder(self):
-        pass
-
-    # Time Complexity: 
-    # Space Complexity:     
-    def height(self):
-        pass
-
-
-#   # Optional Method
-#   # Time Complexity: 
-#   # Space Complexity: 
-    def bfs(self):
-        pass
-
+    def preorder_helper(self, current, values):
+        if current is None:
+            return values
         
+        values.append({
+            "key": current.key,
+            "value": current.value
+        })
+        self.preorder_helper(current.left, values)
+        self.preorder_helper(current.right, values)
+        return values
 
 
-#   # Useful for printing
-    def to_s(self):
-        return f"{self.inorder()}"
+    def postorder_helper(self, current, values):
+        if current is None:
+            return values
+        self.postorder_helper(current.left, values)
+        self.postorder_helper(current.right, values)
+        values.append({
+            "key": current.key,
+            "value": current.value
+        })
+        return values 
+
+
+    def height_helper(self, current):
+        if current == None:
+            return 0
+        left_height = self.height_helper(current.left)
+        right_height = self.height_helper(current.right)
+
+        if left_height > right_height:
+            return left_height + 1
+        else:
+            return right_height + 1
