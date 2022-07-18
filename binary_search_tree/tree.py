7bcdb7f476afdf37ca47a0ef2b94c88e3fafc984
sjolivas <sjolivas@gmail.com>
2022-07-17 19:27:42 -0600

    finally finished BST ... tests passing

--- binary_search_tree/tree.py
+++ binary_search_tree/tree.py
@@ -1,5 +1,11 @@
 class TreeNode:
-    def __init__(self, key, val = None):
+    def __init__(self, key, val=None):
+
+        """
+        key is used to compare values
+        value can be any kind of object
+        """
+
         if val == None:
             val = key
 
@@ -7,53 +13,141 @@ class TreeNode:
         self.value = val
         self.left = None
         self.right = None
-        
 
 
 class Tree:
     def __init__(self):
         self.root = None
 
-    # Time Complexity: 
-    # Space Complexity: 
-    def add(self, key, value = None):
-        pass
+    # Time Complexity: O(log n) if ordered, O(n) if unordered
+    # Space Complexity: O(n)
+    def add(self, key, value=None):
+        node = TreeNode(key, value)
+        if self.root is None:
+            self.root = node
+            return
 
-    # Time Complexity: 
-    # Space Complexity: 
+        current = self.root
+        while current:
+            if current.key > key:
+                # go left
+                if current.left is None:
+                    current.left = node
+                    return
+                else:
+                    current = current.left
+            else:
+                # go right
+                if current.right is None:
+                    current.right = node
+                    return
+                else:
+                    current = current.right
+
+    # Time Complexity: O(log n) if ordered, O(n) if unordered
+    # Space Complexity: O(1)
     def find(self, key):
-        pass
+        if self.root is None:
+            return None
 
-    # Time Complexity: 
-    # Space Complexity: 
+        if self.root.key == key:
+            return self.root.value
+
+        current = self.root
+        while current:
+            if key < current.key:
+                if not current.left:
+                    return None
+                elif current.left.key == key:
+                    return current.left.value
+                current = current.left
+            else:
+                if not current.right:
+                    return None
+                elif current.right.key == key:
+                    return current.right.value
+                current = current.right
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
     def inorder(self):
-        pass
+        node_list = []
+        current = self.root
+        if current == None:
+            return node_list
 
-    # Time Complexity: 
-    # Space Complexity:     
+        while current:
+            node_list.insert(0, {"key": current.key, "value": current.value})
+            current = current.left
+
+        current = self.root.right
+        while current:
+            node_list.append({"key": current.key, "value": current.value})
+            current = current.right
+
+        return node_list
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
     def preorder(self):
-        pass
+        node_list = []
+        current = self.root
+        if current == None:
+            return node_list
+        while current:
+            node_list.append({"key": current.key, "value": current.value})
+            current = current.left
 
-    # Time Complexity: 
-    # Space Complexity:     
+        current = self.root.right
+        while current:
+            node_list.append({"key": current.key, "value": current.value})
+            current = current.right
+        return node_list
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
     def postorder(self):
-        pass
+        node_list = []
+        current = self.root
+        if current == None:
+            return node_list
+        self.postorder_helper(current, node_list)
+        return node_list
 
-    # Time Complexity: 
-    # Space Complexity:     
+    def postorder_helper(self, current, node_list):
+        if current:
+            self.postorder_helper(current.left, node_list)
+            self.postorder_helper(current.right, node_list)
+            node_list.append({"key": current.key, "value": current.value})
+        return node_list
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
     def height(self):
-        pass
+        current = self.root
 
+        if current is None:
+            return 0
 
-#   # Optional Method
-#   # Time Complexity: 
-#   # Space Complexity: 
+        return self.height_helper(current)
+
+    def height_helper(self, current):
+        if current is None:
+            return 0
+
+        left_height = self.height_helper(current.left)
+        right_height = self.height_helper(current.right)
+
+        return max(left_height, right_height) + 1
+
+    # ======================================================================================
+
+    #   # Optional Method
+    #   # Time Complexity:
+    #   # Space Complexity:
     def bfs(self):
         pass
 
-        
-
-
-#   # Useful for printing
+    #   # Useful for printing
     def to_s(self):
         return f"{self.inorder()}"
